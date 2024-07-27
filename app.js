2ad80c96bf79e5eeba3148f63376ca9bb4551925
whatever777 <62002618+whatever777@users.noreply.github.com>
2024-07-27 15:39:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ async function initializeTonConnect() {
     if (typeof TonConnect !== 'undefined') {
         debug('TonConnect SDK loaded successfully');
         try {
-            const connector = new TonConnect.TonConnect({
+            const connector = new TonConnectSDK.TonConnect({
                 manifestUrl: 'https://whatever777.github.io/tons-of-dust-demo/manifest.json'
             });
             debug('TonConnect instance created');
